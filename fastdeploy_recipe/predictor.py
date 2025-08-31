1595cc6a0181fbf3358eb902e39c44d006570ca8
dcebulj <62616113+dcebulj@users.noreply.github.com>
2025-08-31 15:17:36 -0400

    Clean

--- fastdeploy_recipe/predictor.py
+++
@@ -1,9 +0,0 @@
-from nudenet import NudeDetector
-
-detector = NudeDetector()
-
-def predictor(image_paths, batch_size=1):
-    return detector.detect_batch(image_paths, batch_size=batch_size)
-
-
-
